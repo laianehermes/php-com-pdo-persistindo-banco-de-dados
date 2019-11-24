f80900b11fd664f1c3ab3c1c2bbf449fae685c45
laianehermes <laiane.sp@hotmail.com>
2019-11-24 17:07:23 -0300

    Tela de editar carregar os dados reais da categoria selecionada

--- categorias-editar.php
+++ categorias-editar.php
@@ -1,4 +1,13 @@
-<?php require_once 'cabecalho.php' ?>
+<?php 
+
+require_once 'classes/Categoria.php';
+require_once 'cabecalho.php';
+
+$categoria = new Categoria();
+$categoria->id = $_GET['id'];
+$resultado = $categoria->carregar();
+
+?>
 <div class="row">
     <div class="col-md-12">
         <h2>Editar Categoria</h2>
@@ -9,7 +18,7 @@
         <div class="col-md-6 col-md-offset-3">
             <div class="form-group">
                 <label for="nome">Nome da Categoria</label>
-                <input type="text" value="Livros" class="form-control" placeholder="Nome da Categoria">
+                <input type="text" value="<?php echo $resultado['nome'] ?>" class="form-control" placeholder="Nome da Categoria">
             </div>
             <input type="submit" class="btn btn-success btn-block" value="Salvar">
         </div>
--- classes/Categoria.php
+++ classes/Categoria.php
@@ -17,8 +17,24 @@ class Categoria
         return $lista;
     }
 
+    public function carregar() {
+        $query = "SELECT id, nome FROM categorias WHERE id = " . $this->id;
+        $conexao = Conexao::pegarConexao();
+        $resultado = $conexao->query($query);
+        $lista = $resultado->fetchAll();
+        foreach ($lista as $linha) {
+            return $linha;
+        }
+    }
+
     public function inserir($nome) {
         $query = "INSERT INTO categorias (nome) VALUES ('" . $nome . "')";
+        $conexao = Conexao::pegarConexao();
+        $resultado = $conexao->exec($query);
+    }
+
+    public function atualizar() {
+        $query = "UPDATE categorias SET nome = '" . $this->nome . "' WHERE id = " . $this->id;
         $conexao = Conexao::pegarConexao();
         $resultado = $conexao->exec($query);
     }
